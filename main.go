62dfd1f8013dc76874f70873b9b76e8ed6b50c94
WJQSERVER <wjqserver@outlook.com>
2024-09-13 21:19:37 +0800

    0.1.6

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func proxy(c *gin.Context, u string, config *config.Config) {
 		}
 	}
 	req.Header.Del("Host")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36")
 
 	//resp, err := http.DefaultClient.Do(req)
 	client := &http.Client{
